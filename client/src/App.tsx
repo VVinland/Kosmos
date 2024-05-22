23e6638b4504a044b3461835a5de4a976816332c
Daniil <Vinland0_0@mail.ru>
2024-05-22 23:19:40 +0500

    configured webpack for the client

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="app">
+            Рудеус Грейрат
+        </div>
+    );
+}
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,68 @@
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import FilemanagerWebpackPlugin from 'filemanager-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default {
+    mode: 'development',
+    entry: path.join(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index[contenthash:8].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: {
+                            plugins: ["postcss-preset-env"]
+                        }
+                    }
+                }, 'sass-loader']
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', 'jsx', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html'),
+            filename: 'index.html'
+        }),
+        new FilemanagerWebpackPlugin({
+            events: {
+                onStart: {
+                    delete: ['dist']
+                }
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style[contenthash:8].css'
+        })
+    ],
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        port: 3000,
+        open: true,
+        hot: true,
+        liveReload: true
+    }
+}
+
+// export default developmentVersion;
